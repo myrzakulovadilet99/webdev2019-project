f3170dc237e6c0248334986f624825eb27c7edd5
unknown <myrzakulovadilet99>
2019-05-14 09:43:32 +0600

    Adilet

--- project-front/src/app/header/header.component.ts
+++ project-front/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {ProviderService} from '../shared/services/provider.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  // logged: boolean;
+  gymClicked: boolean;
+  login: boolean;
+  registered: boolean;
+  username: string;
+  password: string;
+  password2: string;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  status: string;
+  authlogin: boolean;
+  constructor(private router: Router, private provider: ProviderService) {
   }
 
+  ngOnInit() {
+    // this.logged = this.provider.logged;
+    this.gymClicked = JSON.parse(localStorage.getItem('gymClicked'));
+    this.registered = false;
+    this.username = '';
+    this.password = '';
+    this.password2 = '';
+    this.name = '';
+    this.surname = '';
+    this.email = '';
+    this.phone = '';
+    this.status = '';
+    // localStorage.setItem('login', 'false');
+    // this.login = JSON.parse(localStorage.getItem('login'));
+  }
+  x() {
+    this.authlogin = false;
+    this.registered = false;
+  }
+
+  getAbout() {
+    this.router.navigate([{outlets: {primary: 'about'}}]);
+  }
+  toMainPage() {
+    this.router.navigate([{outlets: {primary: 'login'}}]);
+  }
+  log() {
+    if (JSON.parse(localStorage.getItem('login')) === true) {
+      alert('You already logged');
+    } else {
+      this.authlogin = true;
+    }
+  }
 }
